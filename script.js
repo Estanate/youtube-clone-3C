7257dcb58dc0bfd356a8ea6390269286c7cd9c30
Lucas Romitti <romittikummerlucas@gmail.com>
2026-03-05 16:58:56 -0300

    a

---
+++ script.js
@@ -0,0 +1,12 @@
+const subscribe = document.getElementById('subscribe');
+
+
+let subscribed = false;
+
+function subscribedButtonRender(){
+    if(subscribed === false){
+        subscribe.classList.add('fundo-preto')
+    }
+}
+
+    
