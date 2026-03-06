4be55d1bfab603cc8b464ac5a7bf4e5ba2e51e5b
Estanate <romittikummerlucas@gmail.com>
2026-03-06 17:50:45 -0300

    subscibe button finalizado

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 const subscribe = document.getElementById('subscribe');
+const optionsHeader = document.getElementById('options-header')
 
 
-let subscribed = false;
+let subscribed = JSON.parse(localStorage.getItem('subscribed')) ?? false;
+
+
 
 function subscribedButtonRender(){
-    if(subscribed === false){
+    if(subscribed === true){
         subscribe.style.setProperty('--color-subscribe', 'var(--cinza-escuro)');
         subscribe.style.setProperty('--color-text', 'var(--branco)');
-        subscribe.innerHTML = 'Subscribed <i class="bi bi-bell"></i>';
-        subscribed = true;
+        subscribe.innerHTML = '<i class="bi bi-bell"></i>Subscribed<i class="bi bi-chevron-down"></i>';
     }
     else{
         subscribe.style.setProperty('--color-subscribe', 'var(--branco)');
         subscribe.style.setProperty('--color-text', 'var(--preto)');
         subscribe.innerText = "Subscribe"
+    }
+}
+
+
+function subscribedButtonClicked(){
+    if(subscribed === false){
+        subscribed = true;
+    }
+    else{
         subscribed = false;
     }
-};
+    subscribedButtonRender();
+    localStorage.setItem('subscribed', JSON.stringify(subscribed));
+}
 
-subscribe.addEventListener('click', subscribedButtonRender);
+
+function InitializePage(){
+    subscribedButtonRender();
+}
+
+InitializePage();
+
+subscribe.addEventListener('click', subscribedButtonClicked);
