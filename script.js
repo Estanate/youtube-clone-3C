2f99662077f28984f631e3e29c8c5aae9d82732d
Estanate <romittikummerlucas@gmail.com>
2026-03-06 16:56:06 -0300

    subscribed-button-js

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ let subscribed = false;
 
 function subscribedButtonRender(){
     if(subscribed === false){
-        subscribe.classList.add('fundo-preto')
+        subscribe.style.setProperty('--color-subscribe', 'var(--cinza-escuro)');
+        subscribe.style.setProperty('--color-text', 'var(--branco)');
+        subscribe.innerHTML = 'Subscribed <i class="bi bi-bell"></i>';
+        subscribed = true;
     }
-}
+    else{
+        subscribe.style.setProperty('--color-subscribe', 'var(--branco)');
+        subscribe.style.setProperty('--color-text', 'var(--preto)');
+        subscribe.innerText = "Subscribe"
+        subscribed = false;
+    }
+};
 
-    
+subscribe.addEventListener('click', subscribedButtonRender);
